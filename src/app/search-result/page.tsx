33b7a50545f1f560e13780ffb05a7521a1af9e6f
Poornima Rameshkumar <poornima.rk@gmail.com>
2025-07-20 13:40:30 +0530

    fix: corrected <Product> prop usage for search-result page

--- src/app/search-result/page.tsx
+++ src/app/search-result/page.tsx
@@ -116,15 +116,15 @@ const SearchResult = () => {
                     <div className="list-product-block relative md:pt-10 pt-6">
                         <div className="heading6">Product Search: {query}</div>
                         <div className={`list-product hide-product-sold grid lg:grid-cols-4 sm:grid-cols-3 grid-cols-2 sm:gap-[30px] gap-[20px] mt-5`}>
-                            {currentProducts.map((item) => (
+                            {currentProducts.map((item) =>
                                 item.id === 'no-data' ? (
                                     <div key={item.id} className="no-data-product">
                                         No products match the selected criteria.
                                     </div>
                                 ) : (
-                                    <Product key={item.id} data={item} type='grid' style={{}} />
+                                    <Product key={item.id} data={item} type="grid" className="" />
                                 )
-                            ))}
+                            )}
                         </div>
 
                         {pageCount > 1 && (
